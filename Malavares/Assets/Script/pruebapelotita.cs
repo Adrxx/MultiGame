44764736fe20810e063bf7c2b0a62ef0210048c9
Francisco Geada Rodríguez <frankgeada@mail.com>
2015-09-22 14:05:25 -0500

    Resolucion iphone
    Ajuste de tamaño de la cámara, y código comentado.

--- Malavares/Assets/Script/pruebapelotita.cs
+++ Malavares/Assets/Script/pruebapelotita.cs
@@ -5,8 +5,8 @@ public class pruebapelotita : MonoBehaviour {
 
     private Vector2 vector; //inicializar nuevo vector de velocidad de la pelota
     public Rigidbody2D rb; //declarar el rigidbody de la pelota
-    public Vector2 pos1;
-    public Vector2 pos2;
+    public Vector2 pos1; //declarar el primer pivote para el drag
+    public Vector2 pos2; //declarar el segundo pivote para el drag
     public float escalar =1f;
 
     void Start () {
@@ -15,15 +15,15 @@ public class pruebapelotita : MonoBehaviour {
 	}
 
     void OnMouseDown() {
-        pos1 = Input.mousePosition;
+        pos1 = Input.mousePosition; //toma el primer pivote para el drag
     }
 
     void OnMouseUp() {
         if (rb.isKinematic)
         {
             rb.isKinematic = false; //activa el movimiento
-            pos2 = Input.mousePosition;
-            vector = new Vector2((pos1.x - pos2.x)*escalar,(pos1.y-pos2.y)*escalar);
+            pos2 = Input.mousePosition;  //toma el segundo pivote para el drag
+            vector = new Vector2((pos1.x - pos2.x)*escalar,(pos1.y-pos2.y)*escalar); //crea el vector de velocidad a partir de los pivotes
             rb.velocity = vector; //inicializar velocidad a partir del vector
         }
     }
