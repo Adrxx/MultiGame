ec1c2f26c16328e026e082b83df29d49d2413af3
Francisco Geada Rodríguez <frankgeada@mail.com>
2015-09-22 13:56:32 -0500

    Drag
    Funciona la mecánica de drag para lanzar la bola

--- Malavares/Assets/Script/movercam.cs
+++ Malavares/Assets/Script/movercam.cs
@@ -3,20 +3,20 @@ using System.Collections;
 
 public class movercam : MonoBehaviour {
 
-    public Camera cam;
-    public Transform tracam;
-    public Transform tra;
+    public Camera cam; //objeto camara
+    public Transform tracam; //transform de la camara
+    public Transform tra; //transform de este objeto
 
     // Use this for initialization
     void Start () {
-        cam = (Camera)FindObjectOfType<Camera>(); 
-        tracam = cam.GetComponent<Transform>();
-        tra = this.GetComponent<Transform>();
+        cam = (Camera)FindObjectOfType<Camera>(); //encuentra el objeto camara
+        tracam = cam.GetComponent<Transform>(); //incializa el objeto camara
+        tra = this.GetComponent<Transform>(); //inicializa el transform de este objeto
 	}
 	
 	// Update is called once per frame
 	void Update () {
-       if (tra.position.y>=tracam.position.y+4) tracam.position=new Vector3(tracam.position.x, tra.position.y-4,-10);
+       if (tra.position.y>=tracam.position.y+4) tracam.position=new Vector3(tracam.position.x, tra.position.y-4,-10); //mueve la camara para seguir esta pelota
 	}
 }
  
--- Malavares/Assets/Script/pruebapelotita.cs
+++ Malavares/Assets/Script/pruebapelotita.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class pruebapelotita : MonoBehaviour {
 
-    public Vector2 vector=new Vector2(0,0); //inicializar nuevo vector para pruebas modificable en la pantalla de unity
+    private Vector2 vector; //inicializar nuevo vector de velocidad de la pelota
     public Rigidbody2D rb; //declarar el rigidbody de la pelota
+    public Vector2 pos1;
+    public Vector2 pos2;
+    public float escalar =1f;
 
-	void Start () {
+    void Start () {
         rb= gameObject.GetComponent<Rigidbody2D>(); //inicializar rigid body
-        rb.velocity=vector; //inicializar velocidad a partir del vector
+        rb.isKinematic=true; //cancela el movimiento inicial
 	}
+
+    void OnMouseDown() {
+        pos1 = Input.mousePosition;
+    }
+
+    void OnMouseUp() {
+        if (rb.isKinematic)
+        {
+            rb.isKinematic = false; //activa el movimiento
+            pos2 = Input.mousePosition;
+            vector = new Vector2((pos1.x - pos2.x)*escalar,(pos1.y-pos2.y)*escalar);
+            rb.velocity = vector; //inicializar velocidad a partir del vector
+        }
+    }
 	
 	void Update () {
 	}
