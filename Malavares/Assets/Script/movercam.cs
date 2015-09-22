6d4d22379e3ab85e282f46d6976fb8c79054f757
Francisco Geada Rodríguez <frankgeada@mail.com>
2015-09-22 15:24:14 -0500

    Línea de disparo, optimización de código
    Se implemento una línea para medir la potencia del disparo,  se
    eliminaron variables redundantes en el código.

--- Malavares/Assets/Script/movercam.cs
+++ Malavares/Assets/Script/movercam.cs
@@ -4,19 +4,14 @@ using System.Collections;
 public class movercam : MonoBehaviour {
 
     public Camera cam; //objeto camara
-    public Transform tracam; //transform de la camara
-    public Transform tra; //transform de este objeto
 
-    // Use this for initialization
     void Start () {
         cam = (Camera)FindObjectOfType<Camera>(); //encuentra el objeto camara
-        tracam = cam.GetComponent<Transform>(); //incializa el objeto camara
-        tra = this.GetComponent<Transform>(); //inicializa el transform de este objeto
 	}
 	
-	// Update is called once per frame
+	
 	void Update () {
-       if (tra.position.y>=tracam.position.y+4) tracam.position=new Vector3(tracam.position.x, tra.position.y-4,-10); //mueve la camara para seguir esta pelota
+       if (this.transform.position.y>=cam.transform.position.y+4) cam.transform.position=new Vector3(cam.transform.position.x, this.transform.position.y-4,-10); //mueve la camara para seguir esta pelota
 	}
 }
  
