7d42bb0b9110777b7872fc8b63d508f6a35caf2b
Francisco Geada Rodríguez <frankgeada@mail.com>
2015-09-22 12:29:24 -0500

    Loquequieras

---
+++ Malavares/Assets/Script/movercam.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movercam : MonoBehaviour {
+
+    public Camera cam;
+    public Transform tracam;
+    public Transform tra;
+
+    // Use this for initialization
+    void Start () {
+        cam = (Camera)FindObjectOfType<Camera>(); 
+        tracam = cam.GetComponent<Transform>();
+        tra = this.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (tra.position.y>=tracam.position.y+4) tracam.position=new Vector2(tracam.position.x, (tra.position.y-3)*Time.deltaTime);
+	}
+}
---
+++ Malavares/Assets/Script/pruebapelotita.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pruebapelotita : MonoBehaviour {
+
+    public Vector2 vector=new Vector2(0,0); //inicializar nuevo vector para pruebas modificable en la pantalla de unity
+    public Rigidbody2D rb; //declarar el rigidbody de la pelota
+
+	void Start () {
+        rb= gameObject.GetComponent<Rigidbody2D>(); //inicializar rigid body
+        rb.velocity=vector; //inicializar velocidad a partir del vector
+	}
+	
+	void Update () {
+	}
+}
