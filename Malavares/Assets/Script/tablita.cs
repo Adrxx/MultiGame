fc2b7f8f1cb83f152045a6ad43cd9ff1f25f723e
Francisco Geada Rodríguez <frankgeada@mail.com>
2015-09-23 23:39:20 -0500

    Nueva escena
    Eso

--- Malavares/Assets/Script/tablita.cs
+++ Malavares/Assets/Script/tablita.cs
@@ -2,13 +2,16 @@
 using System.Collections;
 
 public class tablita : MonoBehaviour {
-    public float escalar = .0007f;
+    private int movimiento=2;
 
 	void Start () {
-	
 	}
 	
 	void Update () {
-       //if (Input.GetKey("right")) this.transform.rotation = new Vector2(this.transform.rotation.x + escalar, this.transform.rotation.y);
-	}
-}
+        if (Input.GetKey("down")) transform.Rotate(Vector3.forward * -movimiento);
+        if (Input.GetKey("up")) transform.Rotate(Vector3.forward * movimiento);
+        if (Input.GetKey("left")) transform.position=new Vector3(transform.position.x-.05f, transform.position.y, transform.position.z);
+        if (Input.GetKey("right")) transform.position = new Vector3(transform.position.x+.05f, transform.position.y, transform.position.z);
+    }
+    }
+
